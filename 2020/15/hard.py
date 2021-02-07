bded5c3a079e9df28d9d62877c0ac7e9ec25378e
Urban Pettersson <urbanpe88@gmail.com>
2021-02-07 16:41:02 +0100

    chore: solve 2020 15 part 2 (so slow)

---
+++ 2020/15/hard.py
@@ -0,0 +1,22 @@
+values = map(int, raw_input().strip().split(','))
+
+indexes = {}
+for index, value in enumerate(values):
+  indexes[value] = [index]
+
+index = len(values)
+target = 30000000
+curr = 0
+while index < target - 1:
+  if not curr in indexes:
+    indexes[curr] = []
+    indexes[curr].append(index)
+    curr = 0
+  else:
+    tmp = curr
+    curr = index - indexes[curr][-1]
+    indexes[tmp].append(index)
+
+  index += 1
+
+print curr
