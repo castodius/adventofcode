80e1c42c75e0c46adad7ba967d158a65023ed1a3
Urban Pettersson <urbanpe88@gmail.com>
2021-04-11 20:47:45 +0200

    chore: solve 2020 17 part 2

---
+++ 2020/17/hard.py
@@ -0,0 +1,82 @@
+import sys
+
+ACTIVE = '#'
+INACTIVE = '.'
+CYCLES = 6
+
+class Matrix:
+  def __init__(self, side):
+    self.CENTER=side/2
+    self.SIDE=side
+    self.matrix = [ 
+      [ 
+        [
+          [INACTIVE for _ in range(self.SIDE)]
+            for _ in range(self.SIDE)
+          ]
+          for _ in range(self.SIDE)
+        ]
+        for _ in range(self.SIDE)
+      ]
+
+  def get_center(self):
+    return self.SIDE/2
+
+  def get_side(self):
+    return self.SIDE
+
+  def get_active_neighbours(self, x, y, z, w):
+    deltas = [-1, 0, 1]
+    active = 0
+    for xd in deltas:
+      for yd in deltas:
+        for zd in deltas:
+          for wd in deltas:
+            if xd == 0 and yd == 0 and zd == 0 and wd == 0:
+              continue
+            active += 1 if self.matrix[x+xd][y+yd][z+zd][w+wd] == ACTIVE else 0
+    return active
+
+  def get_active_count(self):
+    count = 0
+    for z in self.matrix:
+      for y in z:
+        for x in y:
+          for w in x:
+            count += 1 if w == ACTIVE else 0
+
+    return count
+
+def init():
+  rows = [row.strip() for row in sys.stdin]
+  n = len(rows[0])
+  matrix = Matrix((n/2+2+CYCLES)*2)
+  SHIFT = matrix.get_center() - n/2
+  for index, row in enumerate(rows):
+    matrix.matrix[SHIFT][SHIFT][SHIFT+index][SHIFT:SHIFT+n] = [char for char in row]
+
+  return matrix
+
+def run_cycle(matrix):
+  side = matrix.get_side()
+  new_matrix = Matrix(matrix.get_side())
+  for x in range(1, side-1):
+    for y in range(1, side-1):
+      for z in range(1, side-1):
+        for w in range(1, side-1):
+          count = matrix.get_active_neighbours(x, y, z, w)
+          if matrix.matrix[x][y][z][w] == ACTIVE:
+            if 2 <= count <= 3:
+              new_matrix.matrix[x][y][z][w] = ACTIVE
+          else:
+            if count == 3:
+              new_matrix.matrix[x][y][z][w] = ACTIVE
+
+  return new_matrix
+
+matrix = init()
+for i in range(CYCLES):
+  print i
+  matrix = run_cycle(matrix)
+
+print matrix.get_active_count()
