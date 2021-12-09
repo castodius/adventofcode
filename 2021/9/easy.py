0c908f01be15f6a21cd1e69ed7eaa19f0229581d
Urban Pettersson <urbanpe88@gmail.com>
2021-12-09 16:06:47 +0100

    chore: solve day 9 part 1

---
+++ 2021/9/easy.py
@@ -0,0 +1,23 @@
+import sys
+
+def solve():
+  lines = [[int(value) for value in line.strip()] for line in sys.stdin]
+  values = []
+  y_len = len(lines)
+  x_len = len(lines[0])
+  for y in range(y_len):
+    for x, value in enumerate(lines[y]):
+      if y > 0 and lines[y-1][x] <= value:
+        continue
+      if y < y_len - 1 and lines[y+1][x] <= value:
+        continue
+      if x > 0 and lines[y][x-1] <= value:
+        continue
+      if x < x_len - 1 and lines[y][x+1] <= value:
+        continue
+      print x, y, value
+      values.append(value)
+  print values
+  return len(values)+sum(values)
+ 
+print solve()
